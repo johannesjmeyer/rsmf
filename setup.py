f77939eaede802ee959b75fc763c3d945049dc2b
johannesjmeyer <jj.meyer@outlook.com>
2020-08-24 16:51:09 +0200

    Update setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+version = "0.1"
+
+requirements = [
+    "matplotlib",
+]
+
+info = {
+    'name': 'rsmf',
+    'version': version,
+    'maintainer': 'Johannes Jakob Meyer',
+    'maintainer_email': 'mail@johannesjakobmeyer.com',
+    'url': 'https://github.com/johannesjmeyer/rsmf',
+    'packages': find_packages(where="."),
+    'description': 'rsmf (right-size my figures) helps you prepare publication-ready figures with matplotlib.',
+    'long_description': open('README.rst').read(),
+    'provides': ["rsmf"],
+    'install_requires': requirements,
+}
+
+setup(**(info))
